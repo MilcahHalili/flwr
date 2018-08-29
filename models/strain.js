c08bc50a444eb6f4a9fa4160da1ab5d77ee9b52d
MilcahHalili <halilicenteno@gmail.com>
2018-08-28 17:28:40 -0700

    add strainSchema post function to strain model

--- models/strain.js
+++ models/strain.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var strainSchema = new mongoose.Schema({
   name: String,
@@ -7,9 +8,21 @@ var strainSchema = new mongoose.Schema({
     category: String,
     mood: String
   },
-  dispensaries: [mongoose.Schema.Types.Dispensaries]
+  dispensaries: String // [{type: Schema.Types.ObjectId, ref: 'Dispensary'}]
 }, {
     timestamps: true
+});
+
+strainSchema.post('remove', function(doc) {
+  this.model('Dispensary').find(
+    {strains: doc._id},
+    function(err, dispensaries) {
+      dispensaries.forEach(function(bar) {
+        dispensary.strains.remove(doc._id);
+        dispensary.save();
+      });
+    }
+  );
 });
 
 module.exports = mongoose.model('Strain', strainSchema);
